03e66d5aaf209d45e70d8b5e9e9e6b020ef97dc8
victor quino <danielhackero@gmail.com>
2020-07-17 12:03:21 +0200

    web form que consume recursos del web service en net

---
+++ WFAlumnoConBD/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WFAlumnoConBD
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            GridView1.DataSource = ws.personaParametros("1");
+            GridView1.DataBind();
+        }
+    }
+}
